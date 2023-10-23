ab7d31eda40af3b8b37b44021b2231d31e1b7a5d
Alexander Dolzhenko <raskalovalexander@gmail.com>
2023-10-23 16:42:38 +0300

    sprint_1 Инициализация проекта

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import handlebars from 'vite-plugin-handlebars';
+
+export default defineConfig({
+//    server: {
+//        port: 3000,
+//    },
+    plugins: [
+        handlebars(),
+        ],
+})
