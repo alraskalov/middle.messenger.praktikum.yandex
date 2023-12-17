cc7d7637c93c8f0a2db8a389e4e1922475daf8fe
Alexander Dolzhenko <raskalovalexander@gmail.com>
2023-12-17 22:05:04 +0300

    sprint_4 renderDOM.ts | LF

--- src/utils/scripts/renderDOM.ts
+++ src/utils/scripts/renderDOM.ts
@@ -1,1 +1,13 @@
-import Block from './Block/Block';function renderDOM(query: string, block: Block) {  const root: HTMLElement = document.querySelector(query) as HTMLElement;  root.prepend(block.getContent());  block.dispatchComponentDidMount();  return root;}export default renderDOM;
+import Block from './Block/Block';
+
+function renderDOM(query: string, block: Block) {
+  const root: HTMLElement = document.querySelector(query) as HTMLElement;
+
+  root.prepend(block.getContent());
+
+  block.dispatchComponentDidMount();
+
+  return root;
+}
+
+export default renderDOM;
